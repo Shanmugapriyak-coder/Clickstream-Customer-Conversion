54eaecaa84f4aaf364d7f0f5b653844b354877c6
Shanmugapriyak-coder <159718480+Shanmugapriyak-coder@users.noreply.github.com>
2025-04-14 01:02:09 +0530

    Update clickstream_analysis.py

--- clickstream_analysis.py
+++ clickstream_analysis.py
@@ -256,7 +256,7 @@ def Purchase_Prediction_form():
                         'order':[order],'country':[country],
                         'page1_main_category':[page1_main_category],'page2_clothing_model':[page2_clothing_model],
                         'colour':[colour],'location':[location],'model_photography':[model_photography],
-                       'page':[page]})
+                       'page':[page],'price':[45]})
 
           
             result=classification_model.predict(df)
